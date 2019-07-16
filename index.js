1a745c6177fba5ca5c2b781f595c3c4bf7f9207f
Ryan Gill <ryanjgill2@gmail.com>
2019-07-15 19:45:09 -0500

    emit toggle over websockets

--- index.js
+++ index.js
@@ -75,104 +75,137 @@ new five.Board({ repl: false }).on("ready", function() {
     socket.emit('misterValue', misterRelayValue);
     socket.emit('lightsValue', lightsRelayValue);
   })
-})
 
-app.use(cors())
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+  app.use(cors())
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }))
 
-// route for main dashboard
-app.get('/', (req, res, next) => {
-  res.render('index')
-  next()
-})
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
 
-// get last measurement
-app.get('/measurement', (req, res, next) => {
-  res.json(lastReading || {})
-  next()
-})
-
-app.get('/measurements', (req, res, next) => {
-  getSampleMeasurements()
-  .then(results => {
-    let temp = results.reduce((measurements, m, index) => {
-      if (index % 10 === 0) { measurements.push(m) }
-      return measurements
-    }, [])
-
-    if (lastReading) {
-      results.push(lastReading);
-    }
-    res.json(temp)
+  // route for main dashboard
+  app.get('/', (req, res, next) => {
+    res.render('index')
     next()
   })
-  .catch(err => {
-    console.log(err)
+
+  // get last measurement
+  app.get('/measurement', (req, res, next) => {
+    res.json(lastReading || {})
     next()
   })
-})
 
-app.get('/sparklines', (req, res, next) => {
-  getSparklines()
-  .then(results => {
-    if (lastReading) {
-      results.push(lastReading);
-    }
-
-    res.json(results)
-    next()
-  })
-  .catch(err => {
-    console.log(err)
-    next()
-  })
-})
-
-// POST temperatures
-app.post('/temperatures', (req, res, next) => {
-  let body = JSON.parse(JSON.stringify(req.body))
-  let reading = {
-    date: new Date(),
-    probeA: body.probeA,
-    probeB: body.probeB,
-    probeC: body.probeC,
-    probeD: body.probeD,
-    probeE: body.probeE,
-    rig_name: body.rig_name,
-    uvIndex: body.uvIndex,
-    humidity: body.humidity,
-    soilMoisture: body.soilMoisture,
-    temperature: body.temperature,
-    misterWaterLevel,
-    drainWaterLevel
-  }
-
-  lastReading = reading
-
-  //console.log(lastReading)
-  //io.sockets.emit('reading', reading)
-
-  res.sendStatus(200)
-  next()
-})
-
-setInterval(() => {
-  if (!lastReading) { return }
-
-  saveMeasurement(lastReading)
+  app.get('/measurements', (req, res, next) => {
+    getSampleMeasurements()
     .then(results => {
-      lastSaveTime = new Date().getTime()
-      console.log('Reading saved.')
+      let temp = results.reduce((measurements, m, index) => {
+        if (index % 10 === 0) { measurements.push(m) }
+        return measurements
+      }, [])
+
+      if (lastReading) {
+        results.push(lastReading);
+      }
+      res.json(temp)
+      next()
     })
-    .catch(error => {
-      console.log(error)
+    .catch(err => {
+      console.log(err)
+      next()
     })
-}, (1000 * 60))
+  })
+
+  app.get('/sparklines', (req, res, next) => {
+    getSparklines()
+    .then(results => {
+      if (lastReading) {
+        results.push(lastReading);
+      }
+
+      res.json(results)
+      next()
+    })
+    .catch(err => {
+      console.log(err)
+      next()
+    })
+  })
+
+  // POST temperatures
+  app.post('/temperatures', (req, res, next) => {
+    let body = JSON.parse(JSON.stringify(req.body))
+    let reading = {
+      date: new Date(),
+      probeA: body.probeA,
+      probeB: body.probeB,
+      probeC: body.probeC,
+      probeD: body.probeD,
+      probeE: body.probeE,
+      rig_name: body.rig_name,
+      uvIndex: body.uvIndex,
+      humidity: body.humidity,
+      soilMoisture: body.soilMoisture,
+      temperature: body.temperature,
+      misterWaterLevel,
+      drainWaterLevel
+    }
+
+    lastReading = reading
+
+    //console.log(lastReading)
+    //io.sockets.emit('reading', reading)
+
+    res.sendStatus(200)
+    next()
+  })
+
+  // Toggle Mister
+  app.post('/toggleMister', (req, res, next) => {
+    // mister relay is active low
+    if (misterRelayValue === true) {
+      misterRelay.on()
+      misterRelayValue = false
+    } else {
+      misterRelay.off()
+      misterRelayValue = true
+    }
+    socket.emit('misterValue', misterRelayValue);
+    res.sendStatus(200)
+    next()
+  })
+
+  // Toggle Lights
+  app.post('/toggleLights', (req, res, next) => {
+    // lights relay is active low
+    if (lightsRelayValue === true) {
+      lightsRelay.on()
+      lightsRelayValue = false
+      
+    } else {
+      lightsRelay.off()
+      lightsRelayValue = true
+    }
+    socket.emit('lightsValue', lightsRelayValue);
+    res.sendStatus(200)
+    next()
+  })
+
+  setInterval(() => {
+    if (!lastReading) { return }
+
+    saveMeasurement(lastReading)
+      .then(results => {
+        lastSaveTime = new Date().getTime()
+        console.log('Reading saved.')
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, (1000 * 60))
+
+})
 
 server.listen(PORT, () => console.log(`API listening on ${serverIP}`))
